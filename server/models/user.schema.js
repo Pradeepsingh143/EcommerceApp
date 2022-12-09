b3939cd3be7678e1331894b593daa5b231e70ad5
pradeep <singhpardeep585@gmail.com>
2022-12-09 14:13:40 +0530

    added more feilds in userschema model

--- server/models/user.schema.js
+++ server/models/user.schema.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-const {schema} = mongoose;
+const {schema, model} = mongoose;
+import AuthRoles from '../utils/authRoles'
 
 const userSchema = schema(
     {
@@ -8,6 +9,29 @@ const userSchema = schema(
             required: [true, "Name is required"],
             maxLenght: [45, "Name must be less than 45"],
             trim : true,
-        }
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minLength: [8, "Password must be at least 8 characters"],
+            select: false
+        },
+        role: {
+            type: String,
+            enum : Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date,
+    },
+    {
+        timestamps: true
     }
 )
+
+module.exports = model('user', userSchema)
