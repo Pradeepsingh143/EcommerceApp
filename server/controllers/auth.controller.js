ef63a72a2d1a79fba8311085dbe2a773b9d28f6d
pradeep <singhpardeep585@gmail.com>
2022-12-13 19:12:58 +0530

    Added login and logout in auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.schema";
 import asyncHandler from "../services/asyncHandler";
 import CustomError from "../utils/customError";
-import cookieOptions from '../utils/cookieOptions'
+import cookieOptions from "../utils/cookieOptions";
+
+
 
 /***********************************************************
  * @SIGNUP
@@ -32,15 +34,75 @@ export const signUp = asyncHandler(async (req, res) => {
     password,
   });
 
-  const token = user.getJwtToken()
+  const token = user.getJwtToken();
   console.log(user);
   user.password = undefined;
 
-  res.cookies("token", token, cookieOptions)
-  res.status(200).json({
+  res.cookies("token", token, cookieOptions);
+  return res.status(200).json({
     success: true,
     token,
-    user
-  })
-
+    user,
+  });
 });
+
+
+
+/***********************************************************
+ * @LOGIN
+ * @Route http://localhost:4000/api/auth/login
+ * @description user login controller for auth a user
+ * @parameter  email, password
+ * @returns user object
+ ***********************************************************/
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    throw new CustomError("All fields are required", 400);
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new CustomError("Invaild credentials", 400);
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (isPasswordMatched) {
+    const token = user.getJwtToken();
+    user.password = undefined;
+    res.cookie("token", token, cookieOptions);
+    return res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
+  }
+
+  throw new CustomError("Invaild credentials", 400);
+});
+
+
+
+/***********************************************************
+ * @LOGOUT
+ * @Route http://localhost:4000/api/auth/logout
+ * @description user logout clear user cookies
+ * @parameter
+ * @returns success message
+ ***********************************************************/
+
+export const logout = asyncHandler(async(_req, res)=>{
+    // Also can use {res.clearCookie("token")}
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.status(200).json({
+        success: true,
+        message: "logged out"
+    })
+})
