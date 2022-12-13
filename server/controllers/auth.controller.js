593feefb39cdf0462ff3f08605a8c3845a368993
pradeep <singhpardeep585@gmail.com>
2022-12-13 23:05:04 +0530

    reset password added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,7 +2,8 @@ import User from "../models/user.schema";
 import asyncHandler from "../services/asyncHandler";
 import CustomError from "../utils/customError";
 import cookieOptions from "../utils/cookieOptions";
-
+import mailHelper from "../utils/mailHelper";
+import crypto from "crypto";
 
 /***********************************************************
  * @SIGNUP
@@ -45,7 +46,6 @@ export const signUp = asyncHandler(async (req, res) => {
   });
 });
 
-
 /***********************************************************
  * @LOGIN
  * @Route http://localhost:4000/api/auth/login
@@ -83,7 +83,6 @@ export const login = asyncHandler(async (req, res) => {
   throw new CustomError("Invaild credentials", 400);
 });
 
-
 /***********************************************************
  * @LOGOUT
  * @Route http://localhost:4000/api/auth/logout
@@ -92,14 +91,104 @@ export const login = asyncHandler(async (req, res) => {
  * @returns success message
  ***********************************************************/
 
-export const logout = asyncHandler(async(_req, res)=>{
-    // Also can use {res.clearCookie("token")}
-    res.cookie("token", null, {
-        expires: new Date(Date.now()),
-        httpOnly: true
-    })
+export const logout = asyncHandler(async (_req, res) => {
+  // Also can use {res.clearCookie("token")}
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "logged out",
+  });
+});
+
+/***********************************************************
+ * @FORGOT_PASSWORD
+ * @Route http://localhost:4000/api/auth/password/forgot
+ * @description forgot password send an token
+ * @parameter email
+ * @returns success message -send email
+ ***********************************************************/
+
+export const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError("user not found", 400);
+  }
+  const resetToken = user.generateForgotPasswordToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetUrl = `${req.protocol}://${req.header(
+    "host"
+  )}/api/auth/password/reset/${resetToken}`;
+
+  const mailMessage = `Your password reset url is \n\n ${resetUrl} \n\n`;
+
+  try {
+    await mailHelper({
+      email: user.email,
+      subject: `Reset password mail for ${req.hostname}`,
+      text: mailMessage,
+    });
     res.status(200).json({
-        success: true,
-        message: "logged out"
-    })
-})
+      succes: true,
+      message: `Email send to ${user.email}`,
+    });
+  } catch (error) {
+    // remove forgotPasswordToken and forgotPasswordExpiry if code failed
+    user.forgotPasswordToken = undefined;
+    user.forgotPasswordExpiry = undefined;
+    user.save({ validateBeforeSave: false });
+    throw new CustomError(error.message || "Email send failed", 400);
+  }
+});
+
+/***********************************************************
+ * @RESET_PASSWORD
+ * @Route http://localhost:4000/api/auth/password/reset/:resetToken
+ * @description User will be able to reset password based on url token
+ * @parameter token from url, password and confirmPassword
+ * @returns user object
+ ***********************************************************/
+
+export const resetPassword = asyncHandler(async (req, res) => {
+  const { token: resetToken } = req.params;
+  const { password, comfirmPassword } = req.body;
+
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    forgotPasswordToken: resetPasswordToken,
+    forgotPasswordExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    throw new CustomError("password token is invaild or expired", 400);
+  }
+
+  if (password !== comfirmPassword) {
+    throw new CustomError("password doesn,t match", 400);
+  }
+
+  user.password = password;
+  user.forgotPasswordToken = undefined;
+  user.forgotPasswordExpiry = undefined;
+
+  await user.save();
+
+  //create token and send as response to user
+  const token = user.getJwtToken();
+  user.password = undefined;
+
+  //helper method for cookie can be added
+  res.cookie("token", token, cookieOptions);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
