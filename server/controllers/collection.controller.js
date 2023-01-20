54aedc05ef2641511f0f42f67f482af2ce4e7094
pradeep singh <singhpardeep585@gmail.com>
2023-01-20 14:03:24 +0530

    added update collection controller

--- server/controllers/collection.controller.js
+++ server/controllers/collection.controller.js
@@ -2,35 +2,72 @@ import CollectionSchema from "../models/collection.schema";
 import asyncHandler from "../services/asyncHandler";
 import CustomError from "../utils/customError";
 
-
 /***********************************************************
  * @createCollection
  * @Route http://localhost:4000/api/create/collection
- * @description user signup controller for creating a new user
- * @parameter name, email, password
- * @returns user object
+ * @description create new collection
+ * @parameter name
+ * @returns success message, collection
  ***********************************************************/
 export const createCollection = asyncHandler(async (req, res) => {
-    const {name: collectionName} = req.body;
+  const { name: collectionName } = req.body;
 
-    if (!collectionName) {
-        throw new CustomError("Collection name is required", 401);
-    }
+  if (!collectionName) {
+    throw new CustomError("Collection name is required", 401);
+  }
 
-    const existCollection = await CollectionSchema.findOne({collectionName})
+  const existCollection = await CollectionSchema.findOne({ collectionName });
 
-    if (existCollection) {
-        throw new CustomError("Collection name already exist in db", 401);
-    }
+  if (existCollection) {
+    throw new CustomError("Collection name already exist in db", 401);
+  }
 
-    const collection = await CollectionSchema.create({
-        collectionName
-    })
-    
-    res.status(200).json({
-        success: true,
-        message: "Collection created successfully",
-        collection
-    })
+  const collection = await CollectionSchema.create({
+    collectionName,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Collection created successfully",
+    collection,
+  });
+});
+
+/***********************************************************
+ * @updateCollection
+ * @Route http://localhost:4000/api/update/collection
+ * @description update collection name
+ * @parameter id: CollectionId
+ * @returns success message, Updatedcollection
+ ***********************************************************/
+export const updateCollection = asyncHandler(async (req, res) => {
+  const { name: collectionName } = req.body;
+  const { id: collectionId } = req.params;
+
+  if (!collectionName) {
+    throw new CustomError("Collection name is required", 401);
+  }
+
+  const existCollection = await CollectionSchema.findOne({ collectionName });
+
+  if (existCollection) {
+    throw new CustomError("Collection name already exist in db", 401);
+  }
+
+  const updatedCollection = await CollectionSchema.findByIdAndUpdate(
+    collectionId,
+    { name: collectionName },
+    { new: true, runValidators: true }
+  );
+
+  if (!updatedCollection) {
+    throw new CustomError("Collection not found", 401);
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Collection created successfully",
+    updatedCollection,
+  });
 });
 
