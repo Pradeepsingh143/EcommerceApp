1c45eda44bb204615196a2498ed922cc13640134
pradeep singh <singhpardeep585@gmail.com>
2023-01-25 01:18:13 +0530

    added profile, passReset, passChange routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 const router = express.Router();
-import {signUp, login, logout, forgotPassword, resetPassword, getProfile} from "../controllers/auth.controller.js"
+import {isLoggedIn} from "../middlewares/auth.middleware.js"
+import {signUp, login, logout, forgotPassword, resetPassword, getProfile, changePassword} from "../controllers/auth.controller.js"
 
 router.post("/signup", signUp);
 router.get("/login", login);
 router.get("/logout", logout);
 router.get("/password/forgot", forgotPassword);
-router.put("/password/reset/:resetToken", resetPassword);
+router.put("/password/reset/:token", resetPassword);
 router.get("/profile", getProfile);
+router.put("/password/change",isLoggedIn, changePassword)
 
 export default router
