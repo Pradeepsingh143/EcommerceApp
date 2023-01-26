8a331d022e3741e243ee68d438e72dc060703181
pradeep singh <singhpardeep585@gmail.com>
2023-01-26 19:51:06 +0530

    added update product, delete controllers

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -9,12 +9,11 @@ import config from "../config.js";
 
 /***********************************************************
  * @createProduct
- * @Route http://localhost:4000/api/product
+ * @Route http://localhost:4000/api/product/create
  * @description create new product
  * @parameter name, price, description, photos, stock, sold, collectionId
  * @returns success message, product object
  ***********************************************************/
-
 export const addProduct = asyncHandler(async (req, res) => {
   const form = formidable({
     multiples: true,
@@ -27,7 +26,7 @@ export const addProduct = asyncHandler(async (req, res) => {
         throw new CustomError(err.message || "something went wrong", 500);
       }
 
-      // generate productId from Mongoose 
+      // generate productId from Mongoose
       let productID = new Mongoose.Types.ObjectId().toHexString();
 
       // check fields
@@ -73,13 +72,14 @@ export const addProduct = asyncHandler(async (req, res) => {
       // if product not created
       if (!product) {
         // * if product got error while creating then delete all images from AWS s3 bucket
-        imageArray.map(async (_file, index) => {
-          await s3DeleteFile({
-            bucketName: config.S3_BUCKET_NAME,
-            key: imageArray[index],
-          });
-        });
-
+        Promise.all(
+          imageArray.map(async (_file, index) => {
+            await s3DeleteFile({
+              bucketName: config.S3_BUCKET_NAME,
+              key: imageArray[index],
+            });
+          })
+        );
         throw new CustomError("product was not created", 400);
       }
 
@@ -100,13 +100,130 @@ export const addProduct = asyncHandler(async (req, res) => {
 });
 
 /***********************************************************
+ * @updateProduct
+ * @Route http://localhost:4000/api/product/update/:id
+ * @description update product
+ * @parameter name, price, description, photos, stock, sold, collectionId
+ * @returns success message, product object
+ ***********************************************************/
+
+// need to work in this controller
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { id: productID } = req.params;
+
+  const form = formidable({
+    multiples: true,
+    keepExtensions: true,
+  });
+
+  form.parse(req, async function (err, fields, files) {
+    try {
+      if (err) {
+        throw new CustomError(err.message || "something went wrong", 500);
+      }
+
+      // handling images
+      let imageArrayResponse = Promise.all(
+        Object.keys(files).map(async (filekey, index) => {
+          const element = files[filekey];
+
+          const data = fs.readFileSync(element.filepath);
+
+          const upload = await s3FileUpload({
+            bucketName: config.S3_BUCKET_NAME,
+            key: `products/${productID}/photos_${index + 1}.png`,
+            body: data,
+            contentType: element.mimetype,
+          });
+          return {
+            secure_url: upload.Location,
+          };
+        })
+      );
+
+      // create an imageArray variable and assign imageArrayResponse
+      const imageArray = await imageArrayResponse;
+
+      const updatedFields = {};
+      Object.keys(fields).forEach((key) => {
+        if (fields[key]) {
+          updatedFields[key] = fields[key];
+        }
+      });
+
+      // create product in db
+      const updatedProduct = await Product.findByIdAndUpdate(
+        productID,
+        {
+          photos: imageArray,
+          ...updatedFields,
+        },
+        {runValidators: false, new: true}
+      );
+
+      // if product not created
+      if (!updatedProduct) {
+        // * if product got error while creating then delete all images from AWS s3 bucket
+        Promise.all(
+          imageArray.map(async (_file, index) => {
+            await s3DeleteFile({
+              bucketName: config.S3_BUCKET_NAME,
+              key: imageArray[index],
+            });
+          })
+        );
+        throw new CustomError("product was not created", 400);
+      }
+
+      //  back response
+      res.status(200).json({
+        success: true,
+        message: "product was create successfully",
+        updatedProduct,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message:
+          error.message || "something went wrong product was not created",
+      });
+    }
+  });
+});
+
+/***********************************************************
+ * @delteProduct
+ * @Route http://localhost:4000/api/product/delete/:id
+ * @description delete product
+ * @parameter id
+ * @returns success message, product object
+ ***********************************************************/
+export const deleteProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    throw new CustomError("Product id is required", 400);
+  }
+  let product = await Product.findByIdAndDelete(id);
+
+  if (!product) {
+    throw new CustomError("Product not found", 400);
+  }
+
+  product = undefined;
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+});
+
+/***********************************************************
  * @getAllProducts
- * @Route http://localhost:4000/api/product
+ * @Route http://localhost:4000/api/product/get
  * @description get all product list
  * @parameter
  * @returns success message, product object
  ***********************************************************/
-
 export const getAllProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
 
@@ -123,12 +240,11 @@ export const getAllProducts = asyncHandler(async (req, res) => {
 
 /***********************************************************
  * @getProductsById
- * @Route http://localhost:4000/api/product/:id
+ * @Route http://localhost:4000/api/product/get/:id
  * @description get all product list
  * @parameter
  * @returns success message, product object
  ***********************************************************/
-
 export const getProductById = asyncHandler(async (req, res) => {
   const { id: productId } = req.params;
   const product = await Product.findById(productId);
