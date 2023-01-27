a136f9d76f4cce0f0e0b014977c6f4c9f214be12
pradeep singh <singhpardeep585@gmail.com>
2023-01-27 19:21:12 +0530

    added cloudnairy to store images

---
+++ server/services/cloudinary.files.js
@@ -0,0 +1,9 @@
+import cloudinary from "../config/cloudinary.config.js";
+
+export const cloudinaryFileUpload = async(file, option)=>{
+    return await cloudinary.uploader.upload(file, option);
+}
+
+export const cloudinaryFileDelete = async(id)=>{
+    return await cloudinary.uploader.destroy(id);
+}
