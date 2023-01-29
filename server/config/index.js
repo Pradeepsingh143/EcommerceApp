8519a04e56afd011c8654e4c8aba3436fbaf5840
pradeep singh <singhpardeep585@gmail.com>
2023-01-29 13:19:03 +0530

    added client side url variable

--- server/config/index.js
+++ server/config/index.js
@@ -26,6 +26,8 @@ const config = {
 
   RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
   RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
+
+  CLIENT_SIDE_URL: process.env.CLIENT_SIDE_URL,
 };
 
 export default config;
