2db246d1f789b048078b2de42d18d44810a90129
pradeep singh <singhpardeep585@gmail.com>
2023-01-30 21:54:38 +0530

    removed suspense and loader from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React, {Suspense, lazy } from "react";
+import React, { lazy } from "react";
 import { RouterProvider } from "react-router-dom";
-import Loader from "./components/Loader.spinner";
 import Router from "./router/router.routes";
 import axios from "axios";
 
@@ -11,9 +10,7 @@ const ProductProvider = lazy(() => import("./context/Product.state"));
 function App() {
   return (
     <ProductProvider>
-       <Suspense fallback={<Loader />}>
       <RouterProvider router={Router} />
-      </Suspense>
     </ProductProvider>
   );
 }
