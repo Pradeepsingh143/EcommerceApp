cbe92099fc11fe3550d4b84898dde73530c1b975
pradeep <singhpardeep585@gmail.com>
2023-03-08 15:53:06 +0530

    changes allowed origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   "http://localhost:4000",
   "http://localhost:5173",
   "https://ecom-admin-dashboard.vercel.app",
-  "https://ecom-admin-dashboard.vercel.app",
+  "https://igniteshark-ecomm.vercel.app",
 ];
 
 export default allowedOrigins;
